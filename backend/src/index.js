6d2ad6402a945587941e9b2c90986e200fd59c1a
Aidenuk <darong0307@gmail.com>
2024-12-21 22:34:16 +0900

    Feat: connect DB with frontend in terms of Auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 import authRouthes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config()
 
@@ -13,6 +14,10 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser()); //cookie parser 은 유저가 담고있는 쿠키를 재해석하거나 활용할 수 있다.decrypt
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
 
 app.use("/api/auth", authRouthes);
 app.use("api/message", messageRoutes);
